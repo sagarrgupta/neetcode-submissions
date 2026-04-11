c893c432b17e58df758c701da8f188be8d6ad672
Sagar Gupta <57631359+sagarrgupta@users.noreply.github.com>
2026-04-11 02:58:35 -0700

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.swift
@@ -0,0 +1,14 @@
+class Solution {
+    func climbStairs(_ n: Int) -> Int {
+        guard n > 1 else { return n }
+        var dp = Array(repeating: 0, count: n)
+        var step1 = 1
+        var step2 = 2
+        for i in 2..<n {
+            let new = step2 + step1
+            step1 = step2
+            step2 = new
+        }
+        return step2
+    }
+}
