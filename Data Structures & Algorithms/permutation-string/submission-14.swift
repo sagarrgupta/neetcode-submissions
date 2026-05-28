3bcdc2256f24dacc14e2998e19f634a07db235b3
Sagar Gupta <57631359+sagarrgupta@users.noreply.github.com>
2026-05-27 19:59:31 -0700

    Add: permutation-string - submission-14

---
+++ Data Structures & Algorithms/permutation-string/submission-14.swift
@@ -0,0 +1,37 @@
+class Solution {
+    func checkInclusion(_ s1: String, _ s2: String) -> Bool {
+        let s1 = Array(s1)
+        let s2 = Array(s2)
+        
+        var need = [Character: Int]()
+        var window = [Character: Int]()
+        var need1 = [Character: Int]()
+        var window1 = [Character: Int]()
+        var need2 = [Character: Int]()
+        var window2 = [Character: Int]()
+        
+        for char in s1 {
+            need[char, default: 0] += 1
+        }
+        
+        let windowSize = s1.count
+        
+        for (index, char) in s2.enumerated() {
+            window[char, default: 0] += 1
+            
+            if index >= windowSize {
+                let leftChar = s2[index - windowSize]
+                window[leftChar]! -= 1
+                if window[leftChar] == 0 {
+                    window.removeValue(forKey: leftChar)
+                }
+            }
+            
+            if window == need {
+                return true
+            }
+        }
+        
+        return false
+    }
+}
