93a4a9efe96aa9837e1c9ea4ef79c63d45eb26b5
Sagar Gupta <57631359+sagarrgupta@users.noreply.github.com>
2026-06-06 19:52:44 -0700

    Add: eating-bananas - submission-6

---
+++ Data Structures & Algorithms/eating-bananas/submission-6.swift
@@ -0,0 +1,31 @@
+class Solution {
+    func minEatingSpeed(_ piles: [Int], _ h: Int) -> Int {
+        var l = 1
+        var r = (piles.first ?? 0)
+        piles.forEach {
+            r = max(r, $0)
+        }
+        
+        var mid = 0
+        var previousMid = mid
+        while l <= r {
+            mid = ((r - l) / 2) + l
+            var count = 0
+            for pile in piles {
+                count += (pile / mid)
+                if (pile % mid) != 0 {
+                    count += 1
+                }
+            }
+            
+            if count > h {
+                l = mid + 1
+            } else {
+                previousMid = mid
+                r = mid - 1
+            }
+        }
+        
+        return previousMid
+    }
+}
