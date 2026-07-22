a78abd5f53ccec517f5ecf76405035c5aaa252e7
Sagar Gupta <57631359+sagarrgupta@users.noreply.github.com>
2026-07-22 15:46:15 -0700

    Add: combination-target-sum - submission-10

---
+++ Data Structures & Algorithms/combination-target-sum/submission-10.swift
@@ -0,0 +1,34 @@
+class Solution {
+    func combinationSum(_ nums: [Int], _ target: Int) -> [[Int]] {
+        var nums = nums.sorted()
+        var sets = [[Int]]()
+        var currentSet = [Int]()
+        var currentSum = 0
+        
+        func dfs(_ i: Int) {
+            if currentSum == target {
+                sets.append(currentSet)
+                return
+            }
+            
+            if currentSum > target || i >= nums.count {
+                return
+            }
+            
+            currentSum += nums[i]
+            currentSet.append(nums[i])
+            dfs(i)
+            
+            let toStopCheckingMore = currentSum > target
+            currentSum -= nums[i]
+            currentSet.removeLast()
+            if !toStopCheckingMore {
+                dfs(i+1)
+            }
+        }
+        
+        dfs(0)
+        
+        return sets
+    }
+}
