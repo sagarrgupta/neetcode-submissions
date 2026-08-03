c151af4da67fd1ef17b59994615411f1f6077270
Sagar Gupta <57631359+sagarrgupta@users.noreply.github.com>
2026-08-03 16:47:36 -0700

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.swift
@@ -0,0 +1,43 @@
+class Solution {
+    func generateParenthesis(_ n: Int) -> [String] {
+        var results = [String]()
+        
+        guard n > 0 else { return results }
+        
+        var open = n
+        var close = n
+        var currentString = ""
+        
+        func backtrack() {
+            print(currentString)
+            guard currentString.count < (2 * n) else {
+                results.append(currentString)
+                return
+            }
+            
+            if open > 0 {
+                currentString.append("(")
+                open -= 1
+                
+                backtrack()
+                
+                currentString.removeLast()
+                open += 1
+            }
+            
+            if close > open {
+                currentString.append(")")
+                close -= 1
+                
+                backtrack()
+                
+                currentString.removeLast()
+                close += 1
+            }
+        }
+        
+        backtrack()
+        
+        return results
+    }
+}
