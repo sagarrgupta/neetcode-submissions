31f5e40636e0f796ec640e2e41de127f438659d4
Sagar Gupta <57631359+sagarrgupta@users.noreply.github.com>
2026-08-08 12:03:47 -0700

    Add: count-number-of-islands - submission-4

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-4.swift
@@ -0,0 +1,34 @@
+class Solution {
+    func numIslands(_ grid: [[Character]]) -> Int {
+        var region = 0
+        var grid = grid
+        
+        let directions = [(-1, 0),
+                        (+1, 0),
+                        (0, -1),
+                        (0, +1)]
+        
+        for (r, row) in grid.enumerated() {
+            for (c, column) in row.enumerated() {
+                if grid[r][c] == "1" {
+                    region += 1
+                    dfs(r, c)
+                }
+            }
+        }
+        
+        func dfs(_ row: Int, _ column: Int) {
+            guard row < grid.count,
+                row >= 0,
+                column < grid[row].count,
+                column >= 0,
+                grid[row][column] == "1" else { return }
+            grid[row][column] = "0"
+            
+            for direction in directions {
+                dfs(row + direction.0, column + direction.1)
+            }
+        }
+        return region
+    }
+}
