98b68903fac4a1aefd56d31c8282249d92e4572f
Sagar Gupta <57631359+sagarrgupta@users.noreply.github.com>
2026-08-07 17:44:11 -0700

    Add: max-area-of-island - submission-2

---
+++ Data Structures & Algorithms/max-area-of-island/submission-2.swift
@@ -0,0 +1,42 @@
+class Solution {
+    // 5:18
+    enum State {
+        case notVisited
+        case visiting
+        case visited
+    }
+
+    func maxAreaOfIsland(_ grid: [[Int]]) -> Int {
+        var maxArea = 0
+        var status = Array(repeating: Array(repeating: State.notVisited, count: grid[0].count), count: grid.count)
+        
+        for (r, row) in grid.enumerated() {
+            for (c, column) in row.enumerated() {
+                if status[r][c] == .notVisited && column == 1 {
+                    maxArea = max(dfs(r, c), maxArea)
+                }
+            }
+        }
+        
+        func dfs(_ r: Int, _ c: Int) -> Int {
+            guard r >= 0 && c >= 0 && r < grid.count && c < grid[r].count else { return 0 }
+            guard grid[r][c] == 1 && status[r][c] == .notVisited else { return 0 }
+            status[r][c] = .visiting
+            
+            let directions = [(-1, 0),
+                            (+1, 0),
+                            (0,-1),
+                            (0,+1)]
+            
+            var toReturn = 1
+            for direction in directions {
+                toReturn += dfs(r + direction.0, c + direction.1)
+            }
+            
+            status[r][c] = .visited
+            return toReturn
+        }
+        
+        return maxArea
+    }
+}
