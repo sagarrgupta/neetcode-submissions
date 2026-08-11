54906bbcb254ee72f1d72f6e0d6c486589b0fff0
Sagar Gupta <57631359+sagarrgupta@users.noreply.github.com>
2026-08-11 01:26:38 -0700

    Add: longest-palindromic-substring - submission-2

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-2.swift
@@ -0,0 +1,39 @@
+// 1:01
+class Solution {
+    func longestPalindrome(_ s: String) -> String {
+        func manacherAlgo() -> String {
+            let tempS = "#" + s.map({ "\($0)#" }).joined()
+            let chars = Array(tempS)
+            var pCount = Array(repeating: 0, count: chars.count)
+            var l = 0
+            var r = 0
+            var longestPalindromeL = 0
+            var longestPalindromeR = 0
+            
+            for i in 0..<chars.count {
+                if i < r {
+                    pCount[i] = min(pCount[l + (r - i)], r - i)
+                }
+                
+                while (i - pCount[i] - 1) >= 0,
+                    (i + pCount[i] + 1) < chars.count,
+                    chars[i - pCount[i] - 1] == chars[i + pCount[i] + 1] {
+                    pCount[i] += 1
+                }
+                
+                if (i + pCount[i]) > r {
+                    l = i - pCount[i]
+                    r = i + pCount[i]
+                }
+                
+                if (r - l + 1) > (longestPalindromeR - longestPalindromeL + 1) {
+                    longestPalindromeL = l
+                    longestPalindromeR = r
+                }
+            }
+            return String(chars[longestPalindromeL...longestPalindromeR]).replacingOccurrences(of: "#", with: "")
+        }
+
+        return manacherAlgo()
+    }
+}
