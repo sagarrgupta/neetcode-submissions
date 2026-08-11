a4a4c18e52403fcd039b2c3af4d127ada1f02046
Sagar Gupta <57631359+sagarrgupta@users.noreply.github.com>
2026-08-10 23:45:37 -0700

    Add: palindromic-substrings - submission-5

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-5.swift
@@ -0,0 +1,41 @@
+// 10:55
+class Solution {
+    func countSubstrings(_ s: String) -> Int {
+        guard s.count > 0 else { return 0 }
+        let chars = Array(s)
+        var memo = Array(repeating:
+                            Array(repeating: false, count: chars.count),
+                        count: chars.count)
+        
+        var totalCount = 0
+        
+        for length in 1...s.count {
+            for i in 0...s.count - length {
+                let j = i + length - 1
+                if length == 1 {
+                    memo[i][j] = true
+                    totalCount += 1
+                    continue
+                }
+                
+                if chars[i] == chars[j] {
+                    if j-i <= 2 {
+                        memo[i][j] = true
+                        totalCount += 1
+                        continue
+                    } else {
+                        if memo[i+1][j-1] {
+                            memo[i][j] = true
+                            totalCount += 1
+                            continue
+                        }
+                    }
+                }
+                
+                memo[i][j] = false
+            }
+        }
+        
+        return totalCount
+    }
+}
