5ec5eb933184fecc9c444c9edc5b84f41a69e493
Sagar Gupta <57631359+sagarrgupta@users.noreply.github.com>
2026-08-11 23:12:27 -0700

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.swift
@@ -0,0 +1,14 @@
+class Solution {
+    func rob(_ nums: [Int]) -> Int {
+        var twoBack = 0
+        var oneBack = 0
+        
+        for money in nums {
+            let current = max(twoBack + money, oneBack)
+            
+            twoBack = oneBack
+            oneBack = current
+        }
+        return oneBack
+    }
+}
