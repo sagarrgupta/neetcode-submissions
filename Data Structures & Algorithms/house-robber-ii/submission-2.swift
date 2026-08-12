8ce0511832fa2e8118d47f7e2f88a7a34eca47fa
Sagar Gupta <57631359+sagarrgupta@users.noreply.github.com>
2026-08-11 23:36:53 -0700

    Add: house-robber-ii - submission-2

---
+++ Data Structures & Algorithms/house-robber-ii/submission-2.swift
@@ -0,0 +1,25 @@
+    class Solution {
+        func rob(_ nums: [Int]) -> Int {
+            guard nums.count > 1 else { return nums.first ?? 0 }
+            
+            var twoBackWithoutEnd = 0
+            var oneBackWithoutEnd = 0
+            
+            for money in 1..<nums.count {
+                let current = max(oneBackWithoutEnd, twoBackWithoutEnd + nums[money])
+                twoBackWithoutEnd = oneBackWithoutEnd
+                oneBackWithoutEnd = current
+            }
+            
+            var twoBackWithEnd = 0
+            var oneBackWithEnd = 0
+            
+            for money in 0..<nums.count-1 {
+                let current = max(oneBackWithEnd, twoBackWithEnd + nums[money])
+                twoBackWithEnd = oneBackWithEnd
+                oneBackWithEnd = current
+            }
+            
+            return max(oneBackWithoutEnd, oneBackWithEnd)
+        }
+}
