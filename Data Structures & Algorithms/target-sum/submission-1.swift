c8b0913fb8ea98a180bf69128c7c488f136370cd
Sagar Gupta <57631359+sagarrgupta@users.noreply.github.com>
2026-08-13 15:49:46 -0700

    Add: target-sum - submission-1

---
+++ Data Structures & Algorithms/target-sum/submission-1.swift
@@ -0,0 +1,27 @@
+class Solution {
+    func findTargetSumWays(_ nums: [Int], _ target: Int) -> Int {
+        var memo = [[Int]: Int]()
+        
+        func backTracking(_ index: Int, _ currentSum: Int) -> Int {
+            guard index < nums.count else {
+                return currentSum == target ? 1 : 0
+            }
+            
+            let key = [index, currentSum]
+            if let value = memo[key] {
+                return value
+            }
+            
+            // positive
+            var toReturn = backTracking(index + 1, currentSum + nums[index])
+            
+            // negative
+            toReturn += backTracking(index + 1, currentSum - nums[index])
+            
+            memo[key] = toReturn
+            return toReturn
+        }
+        
+        return backTracking(0, 0)
+    }
+}
