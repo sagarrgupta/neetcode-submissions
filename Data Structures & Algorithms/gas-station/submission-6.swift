36f3e7043e24d31dd185579daf5cc34ec59c651f
Sagar Gupta <57631359+sagarrgupta@users.noreply.github.com>
2026-08-14 17:28:07 -0700

    Add: gas-station - submission-6

---
+++ Data Structures & Algorithms/gas-station/submission-6.swift
@@ -0,0 +1,32 @@
+class Solution {
+    func canCompleteCircuit(_ gas: [Int], _ cost: [Int]) -> Int {
+        var tank = 0
+        var start = 0
+        
+        if gas.reduce(0, +) < cost.reduce(0, +) {
+            return -1
+        }
+        
+        while start < gas.count {
+            var currentStation = start
+            
+            while currentStation < gas.count {
+                tank += gas[currentStation] - cost[currentStation]
+                
+                if tank < 0 {
+                    start = currentStation + 1
+                    tank = 0
+                    break
+                }
+                
+                currentStation = currentStation + 1
+                
+                if currentStation == gas.count {
+                    return start
+                }
+            }
+        }
+        
+        return -1
+    }
+}
