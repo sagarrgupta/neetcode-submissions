d07c7955811366b4484cc2e41c7edc35d05e1a4e
Sagar Gupta <57631359+sagarrgupta@users.noreply.github.com>
2026-08-14 17:05:02 -0700

    Add: gas-station - submission-3

---
+++ Data Structures & Algorithms/gas-station/submission-3.swift
@@ -0,0 +1,41 @@
+// 4:28
+class Solution {
+    func canCompleteCircuit(_ gas: [Int], _ cost: [Int]) -> Int {
+    var tank = 0
+    
+    var memo = [[Int]: Int]()
+    
+    var stationToConsider = Set(0..<gas.count)
+    
+    while !stationToConsider.isEmpty {
+        let currentStationConsideration = stationToConsider.removeFirst()
+        
+        tank = 0
+        var currentStation = currentStationConsideration
+        var nextStationToGo = currentStation + 1 == gas.count ? 0 : currentStation + 1
+        while true {
+            stationToConsider.remove(currentStation)
+            let key = [currentStation, tank]
+            if let value = memo[key] {
+                break
+            }
+            tank += gas[currentStation]
+            tank = tank - cost[currentStation]
+            
+            if tank < 0 {
+                memo[key] = 1
+                break
+            }
+            
+            currentStation = nextStationToGo
+            nextStationToGo = nextStationToGo + 1 == gas.count ? 0 : nextStationToGo + 1
+            
+            if currentStation == currentStationConsideration {
+                return currentStationConsideration
+            }
+        }
+    }
+    
+    return -1
+}
+}
