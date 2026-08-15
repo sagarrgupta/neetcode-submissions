a14321b3770a9931a51bec451feda33c8b714460
Sagar Gupta <57631359+sagarrgupta@users.noreply.github.com>
2026-08-14 18:59:58 -0700

    Add: partition-labels - submission-0

---
+++ Data Structures & Algorithms/partition-labels/submission-0.swift
@@ -0,0 +1,25 @@
+// 6:36
+class Solution {
+    func partitionLabels(_ s: String) -> [Int] {
+        var toReturn = [Int]()
+        var currentSize = 0
+        var currentStart = 0
+        var hashmap = [Character: Int]()
+        
+        for (index, char) in s.enumerated() {
+            hashmap[char] = index
+        }
+        
+        for (index, char) in s.enumerated() {
+            if let lastIndex = hashmap[char] {
+                currentSize = max(currentSize, lastIndex)
+            }
+            if index == currentSize {
+                toReturn.append(currentSize - currentStart + 1)
+                currentStart = index + 1
+            }
+        }
+        
+        return toReturn
+    }
+}
