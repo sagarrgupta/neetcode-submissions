8ee0137645d21f97ee76b8797d45aaa4a106a64c
Sagar Gupta <57631359+sagarrgupta@users.noreply.github.com>
2026-08-15 18:52:53 -0700

    Add: merge-intervals - submission-0

---
+++ Data Structures & Algorithms/merge-intervals/submission-0.swift
@@ -0,0 +1,26 @@
+// 6:42
+class Solution {
+    func merge(_ intervals: [[Int]]) -> [[Int]] {
+        guard !intervals.isEmpty else { return [[]] }
+        var intervals = intervals.sorted {
+            $0[0] < $1[0]
+        }
+        
+        var res = [[Int]]()
+        var index = 1
+        var currentlyProcessing = intervals[0]
+        while index < intervals.count {
+            let interval = intervals[index]
+            if interval[0] <= currentlyProcessing[1] {
+                currentlyProcessing[1] = max(interval[1], currentlyProcessing[1])
+            } else {
+                res.append(currentlyProcessing)
+                currentlyProcessing = interval
+            }
+            index += 1
+        }
+        res.append(currentlyProcessing)
+        
+        return res
+    } 
+}
