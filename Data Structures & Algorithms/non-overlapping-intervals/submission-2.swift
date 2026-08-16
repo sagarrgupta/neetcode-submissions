c757f9db4b802ef637d105d6c37a861c3575ceef
Sagar Gupta <57631359+sagarrgupta@users.noreply.github.com>
2026-08-15 19:21:16 -0700

    Add: non-overlapping-intervals - submission-2

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-2.swift
@@ -0,0 +1,30 @@
+class Solution {
+    func eraseOverlapIntervals(_ intervals: [[Int]]) -> Int {
+        var erased = 0
+        
+        var intervals = intervals.sorted {
+            if $0[0] == $1[0] {
+                ($0[1] - $0[0]) < ($1[1] - $1[0])
+            } else {
+                $0[0] < $1[0]
+            }
+        }
+        
+        var previous = [Int]()
+        for interval in intervals {
+            if previous.isEmpty {
+                previous = interval
+                continue
+            }
+            
+            if interval[0] < previous[1] {
+                previous[1] = min(previous[1], interval[1])
+                erased += 1
+            } else {
+                previous = interval
+            }
+        }
+        
+        return erased
+    }
+}
